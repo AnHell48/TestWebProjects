04a267993e24229c614a24fdaccd054b3815dacd
Angel <48391525+AnHell48@users.noreply.github.com>
2020-02-06 17:02:10 -0400

    Add files via upload

--- CameraTest/cam.js
+++ CameraTest/cam.js
@@ -1,12 +1,30 @@
 var video = document.getElementById("video");
+var canvas = document.getElementById('canvas');
+var contxt = canvas.getContext("2d");
 
-if(navigator.mediaDevice && navigator.mediaDevice.getUserMedia)
+//take pic
+document.getElementById('snap').addEventListener("click", function()
 {
-  navigator.mediaDevice.getUserMedia({video:true}).then(function(stream)
+
+  contxt.drawImage(video,0,0,640,480);
+});
+
+document.getElementById("convert").addEventListener("click", function()
+{
+
+  var img = new Image();
+  img.src = canvas.toDataURL("image/png");
+  // canvas.width = img.width/2;
+  // canvas.height = img.height/2;
+  // contxt.drawImage(img,0,0,640,480);
+  document.write("<img src='"+img+"' />");
+});
+// have a overlay or a btn and when press it will ask for cam and turn it on
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+{
+  navigator.mediaDevices.getUserMedia({video:true}).then(function(stream)
   {
     video.srcObject = stream;
     video.play();
   });
 }
-
-alert("hello");
