6d91b49a349545b31ee77febf7ba02f3b2511b05
Angel <48391525+AnHell48@users.noreply.github.com>
2020-02-06 20:42:47 -0400

    cam array added

--- CameraTest/cam.js
+++ CameraTest/cam.js
@@ -1,22 +1,19 @@
 var video = document.getElementById("video");
 var canvas = document.getElementById('canvas');
 var contxt = canvas.getContext("2d");
+var image;
+var dv = document.getElementById('devices');
 
 //take pic
 document.getElementById('snap').addEventListener("click", function()
 {
-
   contxt.drawImage(video,0,0,640,480);
+  //convert to image
+  img = canvas.toDataURL("image/png");
 });
 
 document.getElementById("convert").addEventListener("click", function()
 {
-
-  var img = new Image();
-  img.src = canvas.toDataURL("image/png");
-  // canvas.width = img.width/2;
-  // canvas.height = img.height/2;
-  // contxt.drawImage(img,0,0,640,480);
   document.write("<img src='"+img+"' />");
 });
 // have a overlay or a btn and when press it will ask for cam and turn it on
@@ -27,4 +24,12 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
     video.srcObject = stream;
     video.play();
   });
+}
+
+getDevices();
+async function getDevices()
+{
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  dv.innerHTML = devices;
+  console.log(devices);
 }
