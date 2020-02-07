2917829828b665b9ceb356c12022627d4364e6dd
Angel <48391525+AnHell48@users.noreply.github.com>
2020-02-06 21:09:38 -0400

    Update cam.js

--- CameraTest/cam.js
+++ CameraTest/cam.js
@@ -19,9 +19,10 @@ document.getElementById("convert").addEventListener("click", function()
 // have a overlay or a btn and when press it will ask for cam and turn it on
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
 {
-  getDevices();
   navigator.mediaDevices.getUserMedia({video:true}).then(function(stream)
   {
+    
+  getDevices();
     video.srcObject = stream;
     video.play();
   });
