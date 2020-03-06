707db323ef277fe159a99c74b44993151b3e7d03
Angel <48391525+AnHell48@users.noreply.github.com>
2020-03-06 09:47:40 -0400

    facingMode fix

--- CameraTest/cam.js
+++ CameraTest/cam.js
@@ -19,7 +19,7 @@
     {
         width: {min:640, ideal:1280, max:1920},
         height: { min: 480, ideal: 720, max: 1080 },
-        facingMode: { exact: "environment" }
+        facingMode: "environment" 
     }
 };
 
